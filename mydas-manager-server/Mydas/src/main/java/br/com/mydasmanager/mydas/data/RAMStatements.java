49c89fbeea569bad3a05317a4241033daba7a727
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-09 19:15:59 -0200

    Corrige coluna de RAM

--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/data/RAMStatements.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/data/RAMStatements.java
@@ -7,6 +7,6 @@ public class RAMStatements {
             + ", freememory"
             + ", datecapture"
             + ", customerdevice"
-            + ", currentmemoryse)"
+            + ", currentmemoryuse)"
             + "VALUES (?, ?, ?, ?, ?)";
 }
