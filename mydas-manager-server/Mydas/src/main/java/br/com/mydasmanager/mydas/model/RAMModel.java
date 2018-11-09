4602f14dd35f930fe20aa3e095c24a9d58136a9a
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-09 19:03:24 -0200

    Transforma valores em porcentagem

--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/model/RAMModel.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/model/RAMModel.java
@@ -1,26 +1,31 @@
 package br.com.mydasmanager.mydas.model;
 
 import java.sql.Date;
+import oshi.util.FormatUtil;
 
 public class RAMModel extends MainInformation {
 
     public int getDeviceId() {
-        return 1004;
+        return selectDeviceId();
     }
 
-    public long getTotalmemory() {
-        return systemInfo.getHardware().getMemory().getTotal();
+    public double getTotalmemory() {
+        return this.toDouble(systemInfo.getHardware().getMemory().getTotal());
     }
 
-    public long getFreememory() {
-        return systemInfo.getHardware().getMemory().getAvailable();
+    public double getFreememory() {
+        return this.toDouble(systemInfo.getHardware().getMemory().getAvailable());
     }
 
-    public long getCurrentUseMemory() {
-        return this.getTotalmemory()-this.getFreememory();
+    public double getCurrentUseMemory() {
+        return this.toDouble((this.getTotalmemory()-this.getFreememory()));
     }
     
     public Date getDateCapture() {
         return date;
     }
+    
+   private double toDouble(long param){
+       return Double.parseDouble(FormatUtil.formatBytes(param).split(" ")[0].replace(",","."));
+   }
 }
