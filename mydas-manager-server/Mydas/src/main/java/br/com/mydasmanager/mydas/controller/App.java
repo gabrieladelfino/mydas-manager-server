816f7605f3bb8deb9c47182e0597d56fd837a967
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-10 15:48:19 -0200

    Remove classes sem uso

--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/App.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/App.java
@@ -1,8 +1,32 @@
 package br.com.mydasmanager.mydas.controller;
 
+import br.com.mydasmanager.mydas.model.Initialize;
+
 public class App {
 
+    static boolean isRunning = true;
+
     public static void main(String[] args) throws InterruptedException {
-        new InitializeComponents();
+        InitializeComponents();
+    }
+
+    public static void InitializeComponents() {
+        CustomerDevice.insert();
+        loadInformation(Initialize.selectInterval());
+    }
+
+    public static void loadInformation(int interval) {
+
+        try {
+            if (isRunning) {
+                GPU.insert();
+                CPU.insert();
+                RAM.insert();
+                SO.insert();
+            }
+            Thread.sleep(interval);
+        } catch (InterruptedException ex) {
+            ex.getMessage();
+        }
     }
 }
--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/CustomerDevice.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/CustomerDevice.java
@@ -3,7 +3,7 @@ package br.com.mydasmanager.mydas.controller;
 import br.com.mydasmanager.mydas.model.CustomerDeviceModel;
 import br.com.mydasmanager.mydas.data.CustomerDeviceRepository;
 
-public class Device {
+public class CustomerDevice {
 
     public static void insert() {
         CustomerDeviceModel m = new CustomerDeviceModel();
