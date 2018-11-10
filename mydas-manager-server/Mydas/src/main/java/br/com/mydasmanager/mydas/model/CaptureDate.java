244a1568212de8d5cab9dab5f244ed28b867868c
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-10 15:50:05 -0200

    Altera data

--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/model/CaptureDate.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/model/CaptureDate.java
@@ -13,13 +13,12 @@ public class CaptureDate {
 
     static final int HOUR = CALENDAR.get(Calendar.HOUR_OF_DAY);
     static final int MINUTE = CALENDAR.get(Calendar.MINUTE);
-    static final int SECONDS = CALENDAR.get(Calendar.SECOND);
 
     static final int DAY = CALENDAR.get(Calendar.DATE);
     static final int MONTH = CALENDAR.get(Calendar.MONTH);
     static final int YEAR = CALENDAR.get(Calendar.YEAR);
 
     public static String selectDate() {
-        return DAY + "/" + MONTH + "/" + YEAR + "-" + HOUR + ":" + MINUTE + ":" + SECONDS;
+        return DAY + "/" + MONTH + "/" + YEAR + "-" + HOUR + ":" + MINUTE;
     }
 }
