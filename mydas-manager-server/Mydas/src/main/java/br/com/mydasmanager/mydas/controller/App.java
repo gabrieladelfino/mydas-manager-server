47a8e625614be7954a4127bdb6adde93c8706371
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-11 12:30:01 -0200

    Corrige App

--- mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/App.java
+++ mydas-manager-server/Mydas/src/main/java/br/com/mydasmanager/mydas/controller/App.java
@@ -1,20 +1,17 @@
 package br.com.mydasmanager.mydas.controller;
 
 import br.com.mydasmanager.mydas.model.Initialize;
+import java.sql.SQLException;
 
 public class App {
 
     static boolean isRunning = true;
 
-    public static void main(String[] args) {
-        InitializeComponents();
-    }
-
-    public static void InitializeComponents() {
+    public static void main(String[] args) throws SQLException {
         loadInformation(Initialize.selectInterval());
     }
 
-    public static void loadInformation(int interval) {
+    public static void loadInformation(int interval) throws SQLException {
 
         try {
             if (isRunning) {
