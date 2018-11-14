aace8d3492441ebeafad6782fb759abe3e2d47ea
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-14 18:45:00 -0200

    Remove string

--- mydas-manager-server/src/main/java/br/com/mydasmanager/data/CustomerDeviceStatements.java
+++ mydas-manager-server/src/main/java/br/com/mydasmanager/data/CustomerDeviceStatements.java
@@ -3,5 +3,4 @@ package br.com.mydasmanager.data;
 public class CustomerDeviceStatements {
 
     public static final String SELECT_MAX_CUSTOMER_DEVICE_ID = "SELECT MAX(id) FROM customer_device";
-    public static final String SELECT_INTERVAL = "SELECT interval WHERE id = "+SELECT_MAX_CUSTOMER_DEVICE_ID;
 }
