4fe725e319d03f28711241b0f8104a997520d09d
gabrieladelfino <gabriela.delfino@outlook.com.br>
2018-11-25 16:03:13 -0200

    Atualiza leitura

--- mydas-manager-server/src/main/java/br/com/mydasmanager/view/Components.java
+++ mydas-manager-server/src/main/java/br/com/mydasmanager/view/Components.java
@@ -1,39 +1,16 @@
 package br.com.mydasmanager.view;
 
-import br.com.mydasmanager.controller.Initialize;
-import br.com.mydasmanager.data.repository.CPURepository;
 import br.com.mydasmanager.data.repository.DeviceRepository;
-import br.com.mydasmanager.data.repository.GPURepository;
-import br.com.mydasmanager.data.repository.HDRepository;
-import br.com.mydasmanager.data.repository.RAMRepository;
-import br.com.mydasmanager.data.repository.SORepository;
-import br.com.mydasmanager.model.CPUModel;
 import br.com.mydasmanager.model.DeviceModel;
-import br.com.mydasmanager.model.GPUModel;
-import br.com.mydasmanager.model.HDModel;
-import br.com.mydasmanager.model.RAMModel;
-import br.com.mydasmanager.model.SOModel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.BorderFactory;
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -44,21 +21,12 @@ public class Components extends JFrame {
     /**
      * Estrutural components
      */
-    DefaultListModel model = new DefaultListModel();
     JPanel components;
     JButton machine;
-    JList listDevices;
     JPanel panel;
     JScrollPane scroll;
     List<DeviceModel> devices;
 
-    /**
-     * Size and location components
-     */
-    private static final int WIDTH_COMPONENT = 150;
-    private static final int HEIGTH_COMPONENT = 30;
-    private static final int AXIS_X = 20;
-    private static final int AXIS_Y = 20;
 
     /**
      *
@@ -97,7 +65,6 @@ public class Components extends JFrame {
 
     private void getDevicesByCustomer(int customerid) {
 
-        model.removeAllElements();
         devices = DeviceRepository.selectDevices(customerid);
 
         for (int i = 0; i < devices.size(); i++) {
