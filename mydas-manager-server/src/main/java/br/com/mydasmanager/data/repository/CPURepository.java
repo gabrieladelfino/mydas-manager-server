c03d2e302b9186bc74f3314bf0a87eacb8383fef
Aluno Bandtec <github@alunos.bandtec.com.br>
2018-12-05 19:52:32 -0200

    Log

--- mydas-manager-server/src/main/java/br/com/mydasmanager/data/repository/CPURepository.java
+++ mydas-manager-server/src/main/java/br/com/mydasmanager/data/repository/CPURepository.java
@@ -6,8 +6,6 @@ import br.com.mydasmanager.controller.CaptureDate;
 import br.com.mydasmanager.controller.EstruturalLog;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class CPURepository {
 
